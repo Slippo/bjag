d17c47d114826ad8e4755bce9c1368a14aa03703
slippo <gmpowercayman@gmail.com>
2023-10-25 15:47:51 -0400

    updated gitignore

--- path_config.h
+++ path_config.h
@@ -1,2 +1,2 @@
-#define MATERIAL_DIRECTORY "C:\\Users\\nino\\Documents\\University\\Year 4 Term 1\\COMP 3501\\a3"
+#define MATERIAL_DIRECTORY "C:\\Users\\nino\\Documents\\University\\Year 4 Term 1\\COMP 3501\\bjag"
 // change to specify your own location here
