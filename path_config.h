abb303da83d8f9bcc71378996082462bc0662e35
dxlce <laura.jin3@gmail.com>
2023-11-26 19:59:48 -0500

    Fixed movement, added collision, and placed machine parts

--- path_config.h
+++ path_config.h
@@ -1,2 +1,2 @@
 // change to specify your own location here
-#define MATERIAL_DIRECTORY  "C:\\Users\\Joey\\OneDrive\\Documents\\GitHub\\bjag"
+#define MATERIAL_DIRECTORY  "D:\\Carleton\\Third Year\\COMP3501\\Project"
