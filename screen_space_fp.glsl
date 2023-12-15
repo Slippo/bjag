7c1da528f6b5e64aee27c9b753d7380b782506bc
Slip <gmpowercayman@gmail.com>
2023-12-15 17:39:31 -0500

    stress test + screen FX

--- screen_space_fp.glsl
+++ screen_space_fp.glsl
@@ -1,23 +1,36 @@
-#version 130
+#version 400
+in vec2 vs_uv;
 
-// Passed from the vertex shader
-in vec2 uv0;
-
-// Passed from outside
 uniform float timer;
 uniform sampler2D texture_map;
+uniform sampler2D bubble;
 
-float wave_strength = 0.001; // The strength of the water wobbliness. Too high is vomit-inducing...
+// Constants
+float wave_strength = 0.001; // Sine wave strenght; vomit-inducing if > 0.001
+vec4 colour = vec4(0,0,0,1);
+
+// random number from 0 to 1 // e.g. float rand_t = genRandom(timer,713597,376547,616027);
+float genRandom(float seed, int a, int b, int m){
+    float r = fract(mod(seed * a + b, m));
+    return r;
+}
 
 void main() 
 {
-	// Screenspace effect
-	vec2 pos = uv0;
-    pos.x = pos.x + wave_strength*(sin(timer*8.2+8.0*pos.y));
-	pos.y = pos.y;
+	vec2 pos = (vs_uv * 2) - 1; // - 1 to 1
+	//pos.x = pos.x + wave_strength*(sin(timer*4+8.0*pos.y));
+
+	float distance_from_center = sqrt(pow(pos.x, 2) + pow(pos.y, 2));
 	
-	// Output
-	vec4 colour = texture(texture_map, pos);
-	vec4 pixel = vec4(colour.r - 0.1, colour.g - 0.1, colour.b + 0.15, colour.a) ;
-	gl_FragColor = pixel;
+	// inside circle? make it wavy
+	distance_from_center < 0.9 ? pos.x = pos.x + 0.005*sin(timer + 2*pos.y):pos.x;
+	
+	// outside circle ? make black. inside? render scene
+	distance_from_center > 0.9 ? colour = vec4(0,0,0,1):colour = texture(texture_map, 0.5*pos + 0.5);
+	
+
+	// blue tinge only if in the game world
+	distance_from_center < 0.9 ? colour = vec4(colour.r - 0.1, colour.g - 0.1, colour.b + 0.2, colour.a):colour = vec4(0,0,0,1);
+
+	gl_FragColor = colour;
 }
