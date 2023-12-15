bc55d0deeefc185edf6920f7118762037922b70f
Slip <gmpowercayman@gmail.com>
2023-12-15 14:05:32 -0500

    Minimum objects
    Commented out all game objects except for the ground. Fixing ground collisions and movement speed

--- game.cpp
+++ game.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
 #include <time.h>
 #include <sstream>
-
 #include "game.h"
 #include "path_config.h"
 
 namespace game {
-
-    // Some configuration constants
-    // They are written here as global variables, but ideally they should be loaded from a configuration file
-
+    // Configuration constants
     // Main window settings
     const std::string window_title_g = "Bjag";
     const unsigned int window_width_g = 1280;
     const unsigned int window_height_g = 720;
     const bool window_full_screen_g = false;
-
     // Viewport and camera settings
-    float camera_near_clip_distance_g = 0.01;
+    float camera_near_clip_distance_g = 0.05;
     float camera_far_clip_distance_g = 1000.0;
-    float camera_fov_g = 60.0; // Field-of-view of camera (degrees)
+    float camera_fov_g = 60.0; // degrees
     const glm::vec3 viewport_background_color_g(0.5, 0.5, 1.0);
     glm::vec3 camera_position_g(0.0, 5.0, 8.0);
     glm::vec3 camera_look_at_g(0.0, 2.5, 0.0);
@@ -28,16 +23,9 @@ namespace game {
     // Materials 
     const std::string material_directory_g = MATERIAL_DIRECTORY;
 
-    // Manipulator
     Manipulator* manipulator = new Manipulator();
 
-    // Text Renderer
-    //TextRenderer* text_renderer = new TextRenderer();
-
-    Game::Game(void) {
-
-        // Don't do work in the constructor, leave it for the Init() function
-    }
+    Game::Game(void) {}
 
 
     void Game::Init(void) {
@@ -56,7 +44,6 @@ namespace game {
         SoundEngine->play2D((MATERIAL_DIRECTORY + std::string("\\audio\\breakout.mp3")).c_str(), true);
 
     }
-
 
     void Game::InitWindow(void) {
 
@@ -324,10 +311,7 @@ namespace game {
     filename = std::string(MATERIAL_DIRECTORY) + std::string("/Gear.png");
     resman_.LoadResource(Texture, "GearTexture", filename.c_str());
   
-    // PARTICLE EFFECT SECTION
-
-    // Load material to be applied to environment particle effect
-    
+    // PARTICLE FX
     filename = std::string(MATERIAL_DIRECTORY) + std::string("/environment");
     resman_.LoadResource(Material, "ParticleGeyserMaterial", filename.c_str());
 
@@ -485,13 +469,12 @@ void Game::SetupGameScreen(void)
     // Light source ("sun")
     scene_.AddNode(manipulator->ConstructSun(&resman_, glm::vec3(0, 100, 0)));
 
-    PopulateWorld();
+    //PopulateWorld();
 
-  //
-    scene_.AddNode(manipulator->ConstructParticleSystem(&resman_, "SphereParticlesBubbles", "BubbleParticles", "ParticleBubbleMaterial", "BubbleTexture", glm::vec3(0, 3, 0)));
+    //scene_.AddNode(manipulator->ConstructParticleSystem(&resman_, "SphereParticlesBubbles", "BubbleParticles", "ParticleBubbleMaterial", "BubbleTexture", glm::vec3(0, 3, 0)));
 
 
-    // Hydrothermal vents
+    /*/ Hydrothermal vents
     scene_.AddNode(manipulator->ConstructParticleSystem(&resman_, "SphereParticles", "Vent1", "ParticleGeyserMaterial", "BubbleTexture", glm::vec3(-71, 0, -23)));
     scene_.GetNode("Vent1")->Scale(glm::vec3(15.0, 1.0, 15.0));
     scene_.GetNode("Vent1")->SetType(CompositeNode::Type::Vent);
@@ -543,7 +526,7 @@ void Game::SetupGameScreen(void)
 
     scene_.AddNode(manipulator->ConstructStalagmite(&resman_, "Stalagmite7", glm::vec3(87.5, 0, -4.0)));
     scene_.GetNode("Stalagmite7")->Scale(glm::vec3(0.7, 0.7, 0.7));
-  //
+  */
 }
 
 void Game::SetupStartScreen(void)
@@ -636,6 +619,7 @@ void Game::MainLoop(void){
                 //std::cout << camera_.GetPosition().x << ", " << camera_.GetPosition().y << ", " << camera_.GetPosition().z << std::endl;
 
                 // Hydrothermal vent collision switch
+                /*
                 if (int(current_time) % 6 == 0) { // On
                     for (int i = 1; i < 7; i++) {
                         scene_.GetNode("Vent" + std::to_string(i))->SetCollision(1);
@@ -646,6 +630,7 @@ void Game::MainLoop(void){
                         scene_.GetNode("Vent" + std::to_string(i))->SetCollision(0);
                     }
                 }
+                */
             }
 
 
